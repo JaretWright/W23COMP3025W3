4db46d93282a0d28e648c6d464c8eed0f33fce41
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-24 15:20:19 -0500

    Initial commit

---
+++ app/src/main/java/com/constantlearningdad/w23comp3025w3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.constantlearningdad.w23comp3025w3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
