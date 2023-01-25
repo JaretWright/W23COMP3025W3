59f89d1f6ae126f85e2e4d4f0780bc5698bb821d
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-24 20:31:57 -0500

    Final update

--- app/src/main/java/com/constantlearningdad/w23comp3025w3/MainActivity.kt
+++ app/src/main/java/com/constantlearningdad/w23comp3025w3/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.constantlearningdad.w23comp3025w3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.constantlearningdad.w23comp3025w3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //in Java -> private ActivityMainBinding binding;
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.tipCalculatorButton.setOnClickListener {
+            startActivity(Intent(this,TipCalculatorActivity::class.java))
+            Log.i("Lifecycle","TipCalculatorActivity intent created")
+        }
 
         Log.i("Lifecycle","MainActivity.onCreate() called")
     }
--- app/src/main/java/com/constantlearningdad/w23comp3025w3/TipCalculatorActivity.kt
+++ app/src/main/java/com/constantlearningdad/w23comp3025w3/TipCalculatorActivity.kt
@@ -3,14 +3,58 @@ package com.constantlearningdad.w23comp3025w3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SeekBar
+import androidx.core.widget.addTextChangedListener
+import com.constantlearningdad.w23comp3025w3.databinding.ActivityTipCalculatorBinding
+import java.text.NumberFormat
 
 class TipCalculatorActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityTipCalculatorBinding
+    private var initialMealCost = 0.0
+    private var tipPercent = 15
    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tip_calculator)
+        binding = ActivityTipCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         
         Log.i("Lifecycle","TipCalculatorActivity.onCreate() called")
+
+        //If the user changes the meal amount, recalculate the tip
+        binding.mealPreTipEditText.addTextChangedListener {
+            if (!it.isNullOrBlank())
+                initialMealCost = it.toString().toDouble()
+            else
+                initialMealCost = 0.0
+
+            calculateTip()
+        }
+
+        //if the user changes the tip amount, recalculate the tip
+        binding.tipPercentSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, newValue: Int, p2: Boolean) {
+                tipPercent = newValue
+                calculateTip()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+    }
+
+    private fun calculateTip()
+    {
+        var tip = initialMealCost*tipPercent / 100
+        binding.tipPercentTextView.text = "${tipPercent}%"
+
+        val currencyFormat = NumberFormat.getCurrencyInstance()
+        binding.tipTotalTextView.text = currencyFormat.format(tip)
+        binding.amountWithTipTextView.text = currencyFormat.format(tip+initialMealCost)
+
     }
 
     override fun onStart(){
